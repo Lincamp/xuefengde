b3ba0f6a80dcc41a366f9452ce507fc56f3b67a1
g706308 <g706308@WS-WXUE.ad.syniverse.com>
2015-03-23 17:28:10 +0100

    session register deprecated so use _SESSION instead

--- login/login.php
+++ login/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$_SESSION['url'] = $_SERVER['REQUEST_URI'];
+//$_SESSION['url'] = $_SERVER['REQUEST_URI'];
 //if (!session_is_registered(myusername)) {
 //    header("location:../login/login.php");
 //}
@@ -11,7 +11,7 @@ $_SESSION['url'] = $_SERVER['REQUEST_URI'];
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
         <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"></script>
         <link rel="stylesheet" type="text/css" href="../belonging/thousestyle.css">
-        <title></title>
+        <title></title>        
     </head>
     <body>
 
@@ -25,82 +25,74 @@ $_SESSION['url'] = $_SERVER['REQUEST_URI'];
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // username and password sent from Form 
-            $myusername = addslashes($_POST['myusername']);
-            $mypassword = addslashes($_POST['mypassword']);
+        $myusername = addslashes($_POST['myusername']);
+        $mypassword = addslashes($_POST['mypassword']);
 
 ////$result = mysqli_query($con, "SELECT * FROM belonging ORDER BY name, change_date DESC");
 ////$numOfRows = mysqli_num_rows($result);
 
-            $sqlcmd = "SELECT id FROM user WHERE username='$myusername' and password='$mypassword'";
+        $sqlcmd = "SELECT id FROM user WHERE username='$myusername' and password='$mypassword'";
 //$result=mysql_query($sql);
-            $result = mysqli_query($con, $sqlcmd);
+        $result = mysqli_query($con, $sqlcmd);
 
 //$row=mysqli_fetch_array($result);
 ////$active=$row['active'];
-            $count = mysqli_num_rows($result);
+        $count = mysqli_num_rows($result);
 
 // If result matched $myusername and $mypassword, table row must be 1 row
-            if ($count == 1) {
-                session_register("myusername");
-                session_register("mypassword");
-                $_SESSION['login_user'] = $myusername;
+        if ($count == 1) {
+//        session_register("myusername");
+        $_SESSION['myusername']= "myusername"; 
+//        session_register("mypassword");
+        $_SESSION['mypassword']= "mypassword";
+        $_SESSION['login_user'] = $myusername;
 
-                if (isset($_SESSION['url']))
-                    $url = $_SESSION['url']; // holds url for last page visited.
-                else
-                    $url = "/index.php"; // default page for
-
-
-
-                    
+        echo "##" . $_SESSION['url'] . "##";
+        //echo $_SESSION['url'] !== "/login/login.php";
+        if (isset($_SESSION['url']))
+        $url = $_SESSION['url']; // holds url for last page visited.
+        else
+        $url = "../index.php"; // default page for
 // go to previous page
-                header("location:$url");
-//header("location:welcome.php");
-                echo "count: $count <br>";
-            }
-            else {
-                $error = "Your Login Name or Password is invalid";
-                echo $error;
-            }
-        }
-        ?>
-        <table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
-            <tr>
-            <form name="form1" method="post" action="login.php">
-                <td>
-                    <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
-                        <tr>
-                            <td colspan="3"><strong>Member Login </strong></td>
-                        </tr>
-                        <tr>
-                            <td width="78">Username</td>
-                            <td width="6">:</td>
-                            <td width="294"><input name="myusername" type="text" id="myusername"></td>
-                        </tr>
-                        <tr>
-                            <td>Password</td>
-                            <td>:</td>
-                            <td><input name="mypassword" type="password" id="mypassword"></td>
-                        </tr>
-                        <tr>
-                            <td>&nbsp;</td>
-                            <td>&nbsp;</td>
-                            <td><input type="submit" name="Submit" value="Login"></td>
-                        </tr>
-                    </table>
-                </td>
-            </form>
-        </tr>
-    </table>
+        //header("location:$url");
+        header("location:../index.php");
+        
+    //header("location:welcome.php");
+    echo "count: $count <br>";
+    }
+    else {
+    $error = "Your Login Name or Password is invalid";
+    echo $error;
+    }
+    }
+    ?>
+    <table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+        <tr>
+        <form name="form1" method="post" action="login.php">
+            <td>
+                <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+                    <tr>
+                        <td colspan="3"><strong>Member Login </strong></td>
+                    </tr>
+                    <tr>
+                        <td width="78">Username</td>
+                        <td width="6">:</td>
+                        <td width="294"><input name="myusername" type="text" id="myusername"></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td>:</td>
+                        <td><input name="mypassword" type="password" id="mypassword"></td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td><input type="submit" name="Submit" value="Login"></td>
+                    </tr>
+                </table>
+            </td>
+        </form>
+    </tr>
+</table>
 </body>
 </html>
-<?
-/*
-<form action="" method="post">
-<label>UserName :</label>
-<input type="text" name="myusername"/><br />
-<label>Password :</label>
-<input type="password" name="mypassword"/><br/>
-<input type="submit" value=" Submit "/><br />
-</form>*/
-?>
