1a1e9efac63b8bc7792671ac4d8f11edfffbbdba
g706308 <g706308@WS-WXUE.ad.syniverse.com>
2015-05-21 10:43:07 +0200

    track dhl

---
+++ wangshushu/howtotrack.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<link rel="stylesheet" type="text/css" href="thousestyle.css">
+<title>Track Package</title>
+</head>
+<body>
+快递一般使用DHL Economy或者荷兰邮政，一般2至3周能到达国内，冬天可能会比较慢。<br>
+<br>
+
+邮递分德国境内和德国到中国这两段，一般包裹到德国邮局后2、3个工作日后国际段的追踪才会生成。<br>
+<br>
+
+如果使用DHL，包裹寄出几天后DHL网站上之后会自己生成一个号，类似CK729260955DE（UPU Code / Matchcode）<br>
+<img src="img/dhl track.jpg" alt=""/>
+<br>
+将这个号码输入<a href="http://www.17track.net/zh-cn/"  target="_blank">17TRACK</a>网站可以对国内段进行追踪。
+
+<script src="http://xuefeng.de/onewebstatic/b866bb27dc.js"></script>
+</body>
+</html>
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * 高头有个查德国境内滴号，
+然后你名字后面有个号，前面加上960就是国际追踪号
+这两个号都在dhl高头查，中国境内滴号和以前一样
+我只挂眼科 21:08:20 
+国际追踪号查滴时候高头会生成中国境内滴号高头有个查德国境内滴号，
+然后你名字后面有个号，前面加上960就是国际追踪号
+这两个号都在dhl高头查，中国境内滴号和以前一样
+我只挂眼科 21:08:20 
+国际追踪号查滴时候高头会生成中国境内滴号
+ */
+?>
+
+
