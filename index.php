9c330b2d53ed79c4fcd826b51459ed412add2c0f
g706308 <g706308@WS-WXUE.ad.syniverse.com>
2015-06-03 14:49:51 +0200

    bootstrap

--- index.php
+++ index.php
@@ -8,14 +8,19 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     </head>
     <body>
+        <div class="container">
         <?php
         require 'menu.php';
 
 //        session_start();
 //        $_SESSION['url'] = $_SERVER['REQUEST_URI'];
-        
+
         echo (_("I am a lazy programmer."));
         echo "<br>";
         echo (_("Here is a"));
@@ -24,6 +29,8 @@ and open the template in the editor.
         <a href="/belonging/insert.php"><?php echo (_("web page")); ?></a>
 
         <?php echo (_("I wrote to note down anything your want, to help your organize your things, thus to save your energy and time.")); ?> 
+        </div>
+        
         <!-- Start of StatCounter Code for Default Guide -->
         <script type="text/javascript">
             var sc_project = 9104726;
--- wangshushu/howtotrack.php
+++ wangshushu/howtotrack.php
@@ -1,39 +1,47 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-<link rel="stylesheet" type="text/css" href="thousestyle.css">
-<title>Track Package</title>
-</head>
-<body>
-快递一般使用DHL Economy或者荷兰邮政，一般2至3周能到达国内，冬天可能会比较慢。<br>
-<br>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <title>Track Package</title>
+    </head>
+    <body>
+        <div class="container">
+            <br>
+            快递一般使用DHL Economy或者荷兰邮政，一般2至3周能到达国内，根据经验，冬天可能会比较慢。<br>
+            <br>
 
-邮递分德国境内和德国到中国这两段，一般包裹到德国邮局后2、3个工作日后国际段的追踪才会生成。<br>
-<br>
+            邮递分德国境内和德国到中国这两段，一般包裹到德国邮局后2、3个工作日后国际段的追踪才会生成。<br>
+            <br>
 
-如果使用DHL，包裹寄出几天后DHL网站上之后会自己生成一个号，类似CK729260955DE（UPU Code / Matchcode）<br>
-<img src="img/dhl track.jpg" alt=""/>
-<br>
-将这个号码输入<a href="http://www.17track.net/zh-cn/"  target="_blank">17TRACK</a>网站可以对国内段进行追踪。
+            如果使用DHL，包裹寄出几天后DHL网站上之后会自己生成一个号，类似CK729260955DE（UPU Code / Matchcode）<br>
+            <br>
+            <img src="img/dhl track.jpg" alt=""/>
+            <br>
+            <br>
+            将这个号码输入<a href="http://www.17track.net/zh-cn/"  target="_blank">17TRACK</a>网站可以对国内段进行追踪。
 
-<script src="http://xuefeng.de/onewebstatic/b866bb27dc.js"></script>
-</body>
+            <script src="http://xuefeng.de/onewebstatic/b866bb27dc.js"></script>
+
+        </div>
+    </body>
 </html>
 <?php
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  * 高头有个查德国境内滴号，
-然后你名字后面有个号，前面加上960就是国际追踪号
-这两个号都在dhl高头查，中国境内滴号和以前一样
-我只挂眼科 21:08:20 
-国际追踪号查滴时候高头会生成中国境内滴号高头有个查德国境内滴号，
-然后你名字后面有个号，前面加上960就是国际追踪号
-这两个号都在dhl高头查，中国境内滴号和以前一样
-我只挂眼科 21:08:20 
-国际追踪号查滴时候高头会生成中国境内滴号
+  然后你名字后面有个号，前面加上960就是国际追踪号
+  这两个号都在dhl高头查，中国境内滴号和以前一样
+  我只挂眼科 21:08:20
+  国际追踪号查滴时候高头会生成中国境内滴号高头有个查德国境内滴号，
+  然后你名字后面有个号，前面加上960就是国际追踪号
+  这两个号都在dhl高头查，中国境内滴号和以前一样
+  我只挂眼科 21:08:20
+  国际追踪号查滴时候高头会生成中国境内滴号
  */
 ?>
 
