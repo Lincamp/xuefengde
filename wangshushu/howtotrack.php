05c8d40fceff2f12c8ac7985e57ee3c2d2fad180
g706308 <g706308@WS-WXUE.ad.syniverse.com>
2015-07-01 11:30:39 +0200

    update

--- wangshushu/howtotrack.php
+++ wangshushu/howtotrack.php
@@ -11,18 +11,32 @@
     <body>
         <div class="container">
             <br>
-            快递一般使用DHL Economy或者荷兰邮政，一般2至3周能到达国内，根据经验，冬天可能会比较慢。<br>
-            <br>
 
-            邮递分德国境内和德国到中国这两段，一般包裹到德国邮局后2、3个工作日后国际段的追踪才会生成。<br>
-            <br>
-
-            如果使用DHL，包裹寄出几天后DHL网站上之后会自己生成一个号，类似CK729260955DE（UPU Code / Matchcode）<br>
-            <br>
-            <img src="img/dhl track.jpg" alt=""/>
-            <br>
-            <br>
-            将这个号码输入<a href="http://www.17track.net/zh-cn/"  target="_blank">17TRACK</a>网站可以对国内段进行追踪。
+            <ul class="list-group">
+                <li class="list-group-item list-group-item-info">
+                    <p>
+                        快递一般使用DHL Economy或者荷兰邮政，一般2至3周能到达国内，根据经验，冬天可能会比较慢。<br>
+                    </p>
+                    <p>
+                        邮递分德国境内和德国到中国这两段，一般包裹到德国邮局后2、3个工作日后国际段的追踪才会生成。<br>
+                    </p>      
+                </li>
+                <li class="list-group-item list-group-item-danger">
+                    如果使用荷兰邮政，我会给您一个追踪号，类似EA123456789NL，您可以用这个追踪号到
+                    <a href="http://www.11183.com.cn/mailtracking/you_jian_cha_xun.html"  target="_blank">EMS官网</a>
+                    进行查询          
+                </li>
+                <li class="list-group-item list-group-item-warning">
+                    如果使用DHL，包裹寄出几天后DHL网站上之后会自己生成一个号，类似CK729260955DE（UPU Code / Matchcode）<br>
+                    <br>
+                    <img src="img/dhl track.jpg" alt=""/>
+                    <br>
+                    <br>
+                    将这个号码输入
+                    <a href="http://intmail.183.com.cn/"  target="_blank">中国邮政官网</a>或者                    
+                    <a href="http://www.17track.net/zh-cn/"  target="_blank">17TRACK</a>网站可以对国内段进行追踪。
+                </li>               
+            </ul>
 
             <script src="http://xuefeng.de/onewebstatic/b866bb27dc.js"></script>
 
