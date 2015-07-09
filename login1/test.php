3e766989cf3fd5d1eb8094a101ecb80483c680de
g706308 <g706308@WS-WXUE.ad.syniverse.com>
2015-07-09 16:29:15 +0200

    login demo

---
+++ login1/test.php
@@ -0,0 +1,21 @@
+<?php
+$dir = dirname(__FILE__);
+require_once ($dir.'/demo_1.php');
+//require_once ('/login1/demo_1.php');
+?><!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+</br>
+        sfdj;laksfj;d
+    </body>
+</html>
